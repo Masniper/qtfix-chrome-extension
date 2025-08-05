7d8aec999dc0042e1013def2cd535ac06aeab36a
Masniper <mohammad.alii.shahsavari@gmail.com>
2025-08-05 04:16:56 +0330

    Feat: Reload all tabs on shortcut change or extension install/update for immediate effect

--- src/background.js
+++ src/background.js
@@ -1,19 +1,51 @@
-
-chrome.action.onClicked.addListener(() => {
-  chrome.runtime.openOptionsPage();
-});
-
-
 chrome.storage.onChanged.addListener((changes, areaName) => {
   if (areaName === 'sync' && changes.shortcut) {
     chrome.tabs.query({}, (tabs) => {
       tabs.forEach(tab => {
-        chrome.tabs.sendMessage(tab.id, {
-          type: 'SHORTCUT_UPDATED',
-          payload: changes.shortcut.newValue
-        }, () => {
-          if (chrome.runtime.lastError) {
-          }
+        if (tab.id) {
+          chrome.tabs.sendMessage(tab.id, {
+            type: 'SHORTCUT_UPDATED',
+            payload: changes.shortcut.newValue
+          }, () => {
+            if (chrome.runtime.lastError) {
+              console.log("Error sending message to tab:", tab.id, chrome.runtime.lastError.message);
+            }
+          });
+          chrome.tabs.reload(tab.id); 
+        }
+      });
+    });
+  }
+});
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install' || details.reason === 'update') {
+    chrome.storage.sync.get(['shortcut'], (result) => {
+      if (!result.shortcut && details.reason === 'install') {
+        const defaultShortcut = {
+          ctrl: false,
+          alt: false, 
+          shift: true,
+          meta: false, 
+          code: 'Space',
+          keyView: 'Space'
+        };
+
+        chrome.storage.sync.set({ shortcut: defaultShortcut }, () => {
+          console.log('Default shortcut set:', defaultShortcut);
+          chrome.runtime.sendMessage({
+            type: "SHORTCUT_UPDATED",
+            payload: defaultShortcut
+          }, () => {
+            if (chrome.runtime.lastError) {
+              console.log("Error sending default shortcut message to popup:", chrome.runtime.lastError.message);
+            }
+          });
+        });
+      }
+      chrome.tabs.query({}, (tabs) => {
+        tabs.forEach(tab => {
+          if (tab.id) chrome.tabs.reload(tab.id);
         });
       });
     });
